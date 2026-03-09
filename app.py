9c13c4614999eaf3b9cc6e94b736c9cb398dc134
devmastermind-19 <mubashraafzal86@gmail.com>
2026-03-09 13:14:38 +0500

    OF-11 Make Profile Management Features

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("OffTheField app is running...")
 print("This is a placeholder for the actual application code.") 
 print("Adding Job postiong feature...")
+print("Adding Profile management feature...")
